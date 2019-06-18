8fa1330b5f890388aee7dac949a260759eae8aec
RainZhong <1322030678@qq.com>
2019-06-18 20:16:46 +0800

    删了一些print

--- interpreter.py
+++ interpreter.py
@@ -51,16 +51,13 @@ def execute_commands(api, file_name):
                 command = ''
             else:
                 command += line
-            print(line)
     else:
         print('文件不存在')
 
 
 def parse_sql(api, sql):
     sql = sql.replace('\n', ' ').replace('\t', '')
-    # print(sql)
     sql_strs = sql.split()
-    # print(sql_strs)
     command = sql_strs[0].lower()
 
     if command == 'create':
@@ -73,30 +70,22 @@ def parse_sql(api, sql):
             assert start != -1 and end != -1, '非法SQL，缺少括号'
             artribute_table = sql[start + 1:end].split(',')
             assert len(artribute_table) > 0, '缺少属性'
-            # print(table_name)
-            # for c in artribute_table:
-            #    print(c)
             api.create_table(table_name, artribute_table)
 
         elif command_type == 'index':
             assert sql_strs[3] == 'on', 'index非法指令'
             index_name = sql_strs[2]
             table_name = sql_strs[4]
-            # artribute_name = sql_strs[5].replace("(", '').replace(")", '')
             start = sql.find('(')
             end = sql.find(')')
             artribute_name = sql[start + 1:end]
             artribute_name = artribute_name.replace(' ', '')
-            # print(artribute_name)
-            # self.create_index()
             api.create_index(table_name, index_name, artribute_name)
 
     elif command == 'drop':
         command_type = sql_strs[1].lower()
         if command_type == 'index':
             index_name = sql_strs[2]
-            # self.drop_index()
-            # table_name = sql_strs[4]
             api.drop_index(index_name)
         elif command_type == 'table':
             table_name = sql_strs[2]
@@ -110,17 +99,10 @@ def parse_sql(api, sql):
                 start = sql.lower().find('where')
                 conditions = sql[start + 5:]
                 conditions = conditions.split('and')
-                # print(conditions)
-                start = time.time()
                 api.delete_records(table_name, conditions)
-                end = time.time()
-                print('指令耗时: {}s'.format(end - start))
         else:  # 若缺省条件
             conditions = []
-            start = time.time()
             api.delete_records(table_name, conditions)
-            end = time.time()
-            print('指令耗时: {}s'.format(end - start))
 
     elif command == 'insert':
         assert sql_strs[1].lower() == 'into', 'SQL非法指令'
@@ -129,47 +111,35 @@ def parse_sql(api, sql):
         assert start != -1, 'SQL缺少values关键字'
         tuple_str = sql[start + 6:]
         record = list(eval(tuple_str))
-        # print(tuple_str)
-        # print(record)
-        start = time.time()
         api.insert_values(table_name, record)
-        end = time.time()
-        print('指令耗时: {}s'.format(end - start))
 
     elif command == 'select':
         assert sql_strs[1] == '*', 'minisql暂不支持非*查找'
         assert sql_strs[2] == 'from', 'SQL语句缺少from'
         table_name = sql_strs[3]
-        # print(sql_strs)
+        print('Results')
+        print('-------------------')
         if len(sql_strs) > 4:
             if sql_strs[4].lower() == 'where':
                 start = sql.lower().find('where')
                 conditions = sql[start + 5:]
                 conditions = conditions.split('and')
-                # print(conditions)
-                start = time.time()
                 rec_block = api.select_records(table_name, conditions)
-                end = time.time()
-                print('指令耗时: {}s'.format(end - start))
-                # print(len(rec_block))
                 for block_number, record_number in rec_block:
                     record = api.get_record_by_block(table_name, block_number, record_number)
                     if record[0] == 1:
                         for i in range(1, len(record)):
-                            print(record[i], end = ' ')
+                            print(record[i], end='\t')
                         print()
         else:
             conditions = []
-            start = time.time()
             rec_block = api.select_records(table_name, conditions)
-            end = time.time()
-            print('指令耗时: {}s'.format(end - start))
-            # print(len(rec_block))
             for block_number, record_number in rec_block:
                 record = api.get_record_by_block(table_name, block_number, record_number)
                 for i in range(1, len(record)):
-                    print(record[i], end = ' ')
+                    print(record[i], end='\t')
                 print()
+        print('-------------------')
 
     elif command == 'exec':
         file_name = sql_strs[1].replace(' ', '')
